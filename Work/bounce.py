d19c7c9d4b80eda2adef7945846159778cd4e1a3
Yan Xiaoxuan <xiaoxuan.427@gmail.com>
2020-06-09 15:52:40 +0800

    Solutions for Section 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+init_height = 100
+bounce_ratio = 3/5
+height = init_height
+i = 1
+
+while i < 11:
+    height = height * bounce_ratio
+    print(i, round(height,4))
+    i = i + 1
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,33 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+def portfolio_cost(filename):
+	f = open(filename, 'rt')
+	headers = next(f)
+	rows = csv.reader(f)
+	total_cost = 0
+
+	for data_row in rows:
+		try:
+			num_stocks = int(data_row[1])
+			price = float(data_row[2])
+			total_cost = total_cost + num_stocks*price
+		except ValueError:
+			print("Missing value. Skip the stocks for", data_row[0])
+	f.close()
+	return total_cost
+
+if len(sys.argv) == 2:
+	filename = sys.argv[1]
+else:
+	filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
+
